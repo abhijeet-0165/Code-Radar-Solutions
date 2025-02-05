ebf24214f6757fa401bb1c8b9c8bb310f954f0dd
abhijeet-0165 <abhijeet0165.becse24@chitkara.edu.in>
2025-02-05 10:25:48 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -5,7 +5,7 @@ int main(){
     char c;
     scanf("%lf %lf %c",&a,&b,&c);
     if(c=='+'){
-        printf(".0lf",a+b);
+        printf("%.0lf",a+b);
     }
     else if(c=='-'){
         printf("%.0lf",a-b);
